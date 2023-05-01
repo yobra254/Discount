090ebf0738d95300863a1a1ac6db68f2546a5b5f
yobra254 <brayodhis071@gmail.com>
2023-05-01 11:36:09 +0300

    First Commit with working Firebase Authentication

---
+++ android/app/src/main/kotlin/com/example/discount/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.discount
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:discount/screen/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: ElevatedButton(
+        child: Text("Logout"),
+        onPressed: () {
+          FirebaseAuth.instance.signOut().then((value) {
+            print("Signed Out");
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignInScreen()));
+          });
+        },
+      ),
+    ));
+  }
+}
